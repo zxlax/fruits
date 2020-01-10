8b6e5737f5717381caf6ef5f12307ad47a03880d
zxlax <55787851+zxlax@users.noreply.github.com>
2020-01-09 22:03:23 -0800

    Update AssemblyInfo.cs
    s

--- FruitsAndVegApp/Properties/AssemblyInfo.cs
+++ FruitsAndVegApp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FruitsAndVegApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Description")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FruitsAndVegApp")]
